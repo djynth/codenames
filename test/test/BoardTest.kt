9cc2d1b5745d152597bb5a4ead1ec6d9d0365019
Dominic Zirbel <djynth@gmail.com>
2017-12-05 17:31:26 -0500

    Improve board tests

--- test/test/BoardTest.kt
+++ test/test/BoardTest.kt
@@ -10,54 +10,70 @@ import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 
 class BoardTest {
-    private lateinit var board: Board
+    private lateinit var board1: Board
+    private lateinit var board2: Board
 
     @BeforeEach
     fun init() {
-        board = Board(Random(0), Team.RED)
+        board1 = Board(Random(0), Team.RED)
+        board2 = Board(Random(0), Team.BLUE)
     }
 
     @Test
     fun testDefaultState() {
+        assertEquals(Board.ROWS * Board.COLS, Board.totalCards())
+
+        val cards1 = board1.cards()
+        val cards2 = board2.cards()
+
         for (row in 0 until Board.ROWS) {
             for (col in 0 until Board.COLS) {
-                assertFalse(board.isRevealed(Square(row, col)))
+                assertFalse(board1.isRevealed(Square(row, col)))
+                assertEquals(cards1[row][col].word, cards2[row][col].word)
             }
         }
 
-        assertEquals(Board.FIRST_TEAM_CARDS, board.unrevealed(Team.RED))
-        assertEquals(Board.SECOND_TEAM_CARDS, board.unrevealed(Team.BLUE))
-        assertEquals(Board.ASSASSIN_CARDS, board.unrevealed(Team.ASSASSIN))
-        assertEquals(Board.neutralCards(), board.unrevealed(Team.NEUTRAL))
+        assertEquals(Board.FIRST_TEAM_CARDS, board1.unrevealed(Team.RED))
+        assertEquals(Board.SECOND_TEAM_CARDS, board1.unrevealed(Team.BLUE))
+        assertEquals(Board.ASSASSIN_CARDS, board1.unrevealed(Team.ASSASSIN))
+        assertEquals(Board.neutralCards(), board1.unrevealed(Team.NEUTRAL))
 
-        assertEquals(0, board.revealed(Team.RED).size)
-        assertEquals(0, board.revealed(Team.BLUE).size)
-        assertEquals(0, board.revealed(Team.ASSASSIN).size)
-        assertEquals(0, board.revealed(Team.NEUTRAL).size)
+        assertEquals(Board.SECOND_TEAM_CARDS, board2.unrevealed(Team.RED))
+        assertEquals(Board.FIRST_TEAM_CARDS, board2.unrevealed(Team.BLUE))
+        assertEquals(Board.ASSASSIN_CARDS, board2.unrevealed(Team.ASSASSIN))
+        assertEquals(Board.neutralCards(), board2.unrevealed(Team.NEUTRAL))
 
-        assertEquals(Board.ROWS * Board.COLS, board.words().size)
+        assertEquals(0, board1.revealed(Team.RED).size)
+        assertEquals(0, board1.revealed(Team.BLUE).size)
+        assertEquals(0, board1.revealed(Team.ASSASSIN).size)
+        assertEquals(0, board1.revealed(Team.NEUTRAL).size)
+
+        assertEquals(Board.totalCards(), board1.words().size)
+        assertEquals(Board.totalCards(), board1.cards().map { it.size }.sum())
+
+        assertEquals(board1.words(), board2.words())
     }
 
     @Test
     fun testIllegalModificationWords() {
-        val words = board.words()
+        val words = board1.words()
         if (words is MutableSet) {
             words.removeAll(words)
         } else {
             words.toMutableSet().removeAll(words)
         }
 
-        assertEquals(Board.ROWS * Board.COLS, board.words().size)
+        assertEquals(Board.totalCards(), board1.words().size)
     }
 
     @Test
     fun testIllegalModificationCards() {
-        val cards = board.cards()
+        val cards = board1.cards()
         for (i in 0 until cards.size) {
             cards[i] = arrayOf()
         }
 
-        assertEquals(Board.ROWS * Board.COLS, board.cards().map { it.size }.sum())
+        assertEquals(Board.totalCards(), board1.cards().map { it.size }.sum())
     }
 
 }
