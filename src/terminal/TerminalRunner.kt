fd0e8bf0a562be9cdfba0ffb7da0a9f366b6888c
Dominic Zirbel <djynth@gmail.com>
2017-12-16 20:29:16 -0500

    Add ability to input the game seed on the command line

--- src/terminal/TerminalRunner.kt
+++ src/terminal/TerminalRunner.kt
@@ -1,11 +1,25 @@
 package terminal
 
 import core.Game
+import java.util.*
 
 fun main(args: Array<String>) {
     println("Welcome to Codenames!")
 
-    val seed = if (args.isNotEmpty()) args[0].toLong() else 0
+    val seedPhrase = if (args.isEmpty()) {
+        println()
+        println("Enter a number or phrase to use as a random seed (empty for a random seed):")
+        readLine()
+    } else {
+        args[0]
+    }
+
+    val seed = if (seedPhrase == null || seedPhrase.isEmpty()) {
+        Random().nextLong()
+    } else {
+        seedPhrase.hashCode().toLong()
+    }
+
     val game = Game(seed, { TerminalSpymaster(it) }, { TerminalGuesser(it) })
     val winner = game.play()
 
